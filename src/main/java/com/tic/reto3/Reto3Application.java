84fb79f3d43b01c434bea1d2564edbb25cffc0f1
Jorge <jorge.jaramillo.mt@usa.edu.co>
2022-10-16 15:49:06 -0500

    Update Reto3Application.java

--- src/main/java/com/tic/reto3/Reto3Application.java
+++ src/main/java/com/tic/reto3/Reto3Application.java
@@ -1,7 +1,10 @@
 package com.tic.reto3;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class Reto3Application {
